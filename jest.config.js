772f4823179d839d3d13949e90cce1f2e32a968a
stickmy <stickmyc@gmail.com>
2020-07-23 09:45:59 +0800

    add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: [
+		'./test', // jest 扫描的目录
+	],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest', // 哪些文件需要用 ts-jest 执行
+	},
+	testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	// globals: {
+	// 	"ts-jest": {
+	// 		tsConfig: 'config/tsconfig.dev.json',
+	// 	},
+	// },
+};
